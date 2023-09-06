6e45153a5cd6f190a6404a5601fd9289968d9597
ville23023 <ville.kylliainen@student.hamk.fi>
2023-09-06 09:41:07 +0300

    Pohjatyö tehty

--- src/ArvausPeli.java
+++ src/ArvausPeli.java
@@ -1,8 +1,26 @@
-
+import java.util.Scanner;
 public class ArvausPeli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		String answer;
+		answer="Terhi";
+		
+		System.out.println("Guess my name.");
+		answer = in.nextLine();
+		
+		if(!answer.equals("Terhi")) {
+			System.out.println("You are wrong!");
+		}
+		else {
+			System.out.println("You are right!");
+		}
+		
+		
+		
+		
 
 	}
 
