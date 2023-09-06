f7c626b996a4908db63bbb9b15bf18c9a70868e4
ville23023 <ville.kylliainen@student.hamk.fi>
2023-09-06 10:15:53 +0300

    Laskuri
    Nyt se laskee arvausten määrät

--- src/ArvausPeli.java
+++ src/ArvausPeli.java
@@ -7,16 +7,22 @@ public class ArvausPeli {
 		
 		String answer;
 		answer="Terhi";
+		int guess;
+		guess=0;
+		
 		
 		System.out.println("Guess my name.");
 		do {
-			
+			guess++;
 			answer = in.nextLine();
 			if(!answer.equals("Terhi")) {
 				System.out.println("You are wrong!Guess again!");
+				
 			}
 			else{
 				System.out.println("You are right!");
+				System.out.println("You guessed "+guess+" times");
+				
 			}
 		}while (!answer.equals("Terhi"));
 		
